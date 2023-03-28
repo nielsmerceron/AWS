4deab8aeb040b90c50e8021479be159606bc2b90
nielsmerceron <nielsmpro@gmail.com>
2023-03-28 21:44:46 +0200

    semblant de todolist on peut rajouter des taches mais il manque la date et la manière de ce log

--- Front/todolist/src/App.svelte
+++ Front/todolist/src/App.svelte
@@ -1,13 +1,54 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+	let todos = [
+		{ done: false, text: 'finish Svelte tutorial' },
+		{ done: false, text: 'build an app' },
+		{ done: false, text: 'world domination' }
+	];
+
+	function add() {
+		todos = todos.concat({ done: false, text: '' });
+	}
+
+	function clear() {
+		todos = todos.filter(t => !t.done);
+	}
+
+	$: remaining = todos.filter(t => !t.done).length;
 </script>
 
-<main>
- <h1> bonjour, présentez vous! </h1>
-</main>
+<h1>Todos</h1>
+
+{#each todos as todo}
+	<div class:done={todo.done}>
+		<input
+			type=checkbox
+			bind:checked={todo.done}
+		>
+
+		<input
+			placeholder="What needs to be done?"
+			bind:value={todo.text}
+		>
+	</div>
+{/each}
+
+<p>{remaining} remaining</p>
+
+<button on:click={add}>
+	Add new
+</button>
+
+<button on:click={clear}>
+	Clear completed
+</button>
 
 <style>
- 
+	.done {
+		opacity: 0.4;
+	}
 </style>
+<main>
+  
+</main>
+
