d9369b6e1eb9d87e90552e915ea780d8059b7d22
AlexisGuigal <alexis.guigal@laposte.net>
2023-03-29 21:26:21 +0200

    add  rapport chef de projet

--- Front/todolist/src/App.svelte
+++ Front/todolist/src/App.svelte
@@ -1,93 +1,59 @@
 <script>
-  let todos = [
-    { done: false, text: 'work'}
-  ];
 
-  function add() {
-    todos = todos.concat({ done: false, text: '' });
-  }
+	let todos = [
+		{ done: false, text: 'work'}
+	];
 
-  function clear() {
-    todos = todos.filter(t => !t.done);
-  }
+	function add() {
+		todos = todos.concat({ done: false, text: '' });
+	}
 
-  function signIn() {
-    // Ajouter le code pour connecter l'utilisateur ici
-    alert('You clicked Sign in');
-  }
+	function clear() {
+		todos = todos.filter(t => !t.done);
+	}
 
-  $: remaining = todos.filter(t => !t.done).length;
+	$: remaining = todos.filter(t => !t.done).length;
 </script>
 
-<main>
-  <h1>Todos</h1>
-  <div>
-    <button on:click={() => alert('You clicked Log in')}>
-      Log in
-    </button>
+<button >
+  Log in
+</button>
 
-    <button on:click={signIn}>
-      Sign in
-    </button>
-  </div>
+<button >
+  Sign in
+</button>
 
-  {#each todos as todo}
-    <div class:done={todo.done}>
-      <input type=checkbox bind:checked={todo.done}>
-      <input placeholder="What needs to be done?" bind:value={todo.text}>
-    </div>
-  {/each}
+<h1>Todos</h1>
+{#each todos as todo}
+	<div class:done={todo.done}>
+		<input
+			type=checkbox
+			bind:checked={todo.done}
+		>
+		<input
+			placeholder="What needs to be done?"
+			bind:value={todo.text}
+		>
+	</div>
+{/each}
 
-  <p>{remaining} remaining</p>
+<p>{remaining} remaining</p>
 
-  <div>
-    <button on:click={add}>Add new</button>
-    <button on:click={clear}>Clear completed</button>
-  </div>
-</main><script>
-  let todos = [
-    { done: false, text: 'work'}
-  ];
+<button on:click={add}>
+	Add new
+</button>
 
-  function add() {
-    todos = todos.concat({ done: false, text: '' });
-  }
+<button on:click={clear}>
+	Clear completed
+</button>
 
-  function clear() {
-    todos = todos.filter(t => !t.done);
-  }
-
-  function signIn() {
-    // Ajouter le code pour connecter l'utilisateur ici
-    alert('You clicked Sign in');
-  }
-
-  $: remaining = todos.filter(t => !t.done).length;
-</script>
+<style>
+	.done {
+		opacity: 0.4;
+	}
+</style>
 
 <main>
-  <h1>Todos</h1>
-  <div>
-    <button on:click={() => alert('You clicked Log in')}>
-      Log in
-    </button>
-
-    <button on:click={signIn}>
-      Sign in
-    </button>
-  </div>
-
-  {#each todos as todo}
-    <div class:done={todo.done}>
-      <input type=checkbox bind:checked={todo.done}>
-      <input placeholder="What needs to be done?" bind:value={todo.text}>
-    </div>
-  {/each}
-
-  <p>{remaining} remaining</p>
-
-  <div>
-    <button on:click={add}>Add new</button>
-    <button on:click={clear}>Clear completed</button>
-  </div>
+  
 </main>
+
