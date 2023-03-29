8eeaafae1da8cc4c31ebcb48ff9d65e630c63335
manelaz <azgagmanel@gmail.com>
2023-03-29 20:03:35 +0200

    ajout image dans src

--- Front/todolist/src/App.svelte
+++ Front/todolist/src/App.svelte
@@ -1,59 +1,47 @@
 <script>
+  let todos = [
+    { done: false, text: 'work'}
+  ];
 
-	let todos = [
-		{ done: false, text: 'work'}
-	];
+  function add() {
+    todos = todos.concat({ done: false, text: '' });
+  }
 
-	function add() {
-		todos = todos.concat({ done: false, text: '' });
-	}
+  function clear() {
+    todos = todos.filter(t => !t.done);
+  }
 
-	function clear() {
-		todos = todos.filter(t => !t.done);
-	}
+  function signIn() {
+    // Ajouter le code pour connecter l'utilisateur ici
+    alert('You clicked Sign in');
+  }
 
-	$: remaining = todos.filter(t => !t.done).length;
+  $: remaining = todos.filter(t => !t.done).length;
 </script>
 
-<button >
-  Log in
-</button>
-
-<button >
-  Sign in
-</button>
-
-<h1>Todos</h1>
-{#each todos as todo}
-	<div class:done={todo.done}>
-		<input
-			type=checkbox
-			bind:checked={todo.done}
-		>
-		<input
-			placeholder="What needs to be done?"
-			bind:value={todo.text}
-		>
-	</div>
-{/each}
-
-<p>{remaining} remaining</p>
-
-<button on:click={add}>
-	Add new
-</button>
-
-<button on:click={clear}>
-	Clear completed
-</button>
-
-<style>
-	.done {
-		opacity: 0.4;
-	}
-</style>
-
 <main>
-  
-</main>
+  <h1>Todos</h1>
+  <div>
+    <button on:click={() => alert('You clicked Log in')}>
+      Log in
+    </button>
 
+    <button on:click={signIn}>
+      Sign in
+    </button>
+  </div>
+
+  {#each todos as todo}
+    <div class:done={todo.done}>
+      <input type=checkbox bind:checked={todo.done}>
+      <input placeholder="What needs to be done?" bind:value={todo.text}>
+    </div>
+  {/each}
+
+  <p>{remaining} remaining</p>
+
+  <div>
+    <button on:click={add}>Add new</button>
+    <button on:click={clear}>Clear completed</button>
+  </div>
+</main>
