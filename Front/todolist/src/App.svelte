5a776b1097a66c374b25c3a0a30e4fca4dec26c4
manelaz <azgagmanel@gmail.com>
2023-03-29 20:06:14 +0200

    ajout des fonction pour sing in et log in

--- Front/todolist/src/App.svelte
+++ Front/todolist/src/App.svelte
@@ -44,4 +44,50 @@
     <button on:click={add}>Add new</button>
     <button on:click={clear}>Clear completed</button>
   </div>
+</main><script>
+  let todos = [
+    { done: false, text: 'work'}
+  ];
+
+  function add() {
+    todos = todos.concat({ done: false, text: '' });
+  }
+
+  function clear() {
+    todos = todos.filter(t => !t.done);
+  }
+
+  function signIn() {
+    // Ajouter le code pour connecter l'utilisateur ici
+    alert('You clicked Sign in');
+  }
+
+  $: remaining = todos.filter(t => !t.done).length;
+</script>
+
+<main>
+  <h1>Todos</h1>
+  <div>
+    <button on:click={() => alert('You clicked Log in')}>
+      Log in
+    </button>
+
+    <button on:click={signIn}>
+      Sign in
+    </button>
+  </div>
+
+  {#each todos as todo}
+    <div class:done={todo.done}>
+      <input type=checkbox bind:checked={todo.done}>
+      <input placeholder="What needs to be done?" bind:value={todo.text}>
+    </div>
+  {/each}
+
+  <p>{remaining} remaining</p>
+
+  <div>
+    <button on:click={add}>Add new</button>
+    <button on:click={clear}>Clear completed</button>
+  </div>
 </main>
