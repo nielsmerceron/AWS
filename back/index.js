623658763dcc10207f19d5fe3dc8432395e97547
Kyuzial <kyuzial@protonmail.com>
2023-04-04 17:44:59 +0200

    Add Connection to db in index.js

--- back/index.js
+++ back/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./db");
+
+InitiateMongoServer();
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("AWS back");
+  res.json({ message: "AWS Back" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Back listening on port ${port}!`);
+  console.log(`127.0.0.1:${port}`);
 });
