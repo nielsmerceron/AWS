4f8a2841b857eb94dc148535609ec47afd80107e
nielsmerceron <nielsmpro@gmail.com>
2023-04-06 18:21:23 +0200

    nettoyage du code d'élément inutile

--- Front/todolist/.svelte-kit/ambient.d.ts
+++ Front/todolist/.svelte-kit/ambient.d.ts
@@ -94,6 +94,7 @@ declare module '$env/static/private' {
 	export const MAIL: string;
 	export const npm_node_execpath: string;
 	export const npm_config_engine_strict: string;
+	export const OLDPWD: string;
 	export const NODE_ENV: string;
 }
 
@@ -192,6 +193,7 @@ declare module '$env/dynamic/private' {
 		MAIL: string;
 		npm_node_execpath: string;
 		npm_config_engine_strict: string;
+		OLDPWD: string;
 		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: string]: string | undefined;
--- Front/todolist/src/routes/signin/+page.svelte
+++ Front/todolist/src/routes/signin/+page.svelte
@@ -14,23 +14,6 @@
 
 <div class="navbar">
   <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class=" lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h8m-8 6h16"
-          /></svg
-        >
-      </label>
-    </div>
     <a class=" normal-case text-xl">To do list</a>
   </div>
   <div class="navbar-center secondary lg:flex">Sign in process</div>
