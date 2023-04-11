f57c5173add57f540109e503cdbc8adf3e583ab0
nielsmerceron <nielsmpro@gmail.com>
2023-04-11 11:50:46 +0200

    ajout des verification de mot de passe et pseudo. je dois changer les regex car fonctionnement random

--- Front/todolist/src/routes/signin/validate.js
+++ Front/todolist/src/routes/signin/validate.js
@@ -7,4 +7,31 @@ function verificationemail(value) {
   return (regex.test(value));
 }
 
-export  { verificationemail}
+
+/**
+ * @param {string} mdp1
+ */
+function verificationmdp(mdp1) {
+  let strongregex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$")
+  return(strongregex.test(mdp1))
+}
+
+/**
+ * @param {string} mdp1
+ * @param {string} mdp2
+ */
+function mdp1equalmdp2(mdp1,mdp2){
+  return(mdp1 != mdp2)
+}
+
+/**
+ * @param {string} pseudo
+ */
+function verificationpseudo(pseudo) {
+  let regex = new RegExp("((/^[A-Za-z])+[A-Za-z0-9_]*){2,16}$")
+  return(regex.test(pseudo))
+}
+
+
+
+export  { verificationemail,verificationmdp,mdp1equalmdp2,verificationpseudo}
