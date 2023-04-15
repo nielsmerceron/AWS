c8cdb637d7975f4e3d9c9aadc63c41629e7eb661
Kyuzial <kyuzial@protonmail.com>
2023-04-16 01:07:15 +0200

    Add todo schema

---
+++ back/src/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
