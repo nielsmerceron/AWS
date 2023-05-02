c4ea5e533436bb2327457c51d7285a35c27834ed
Kyuzial <kyuzial@protonmail.com>
2023-05-02 22:26:20 +0200

    front: add a class with padding for createtodo

--- Front/todolist/src/routes/createtodo/+page.svelte
+++ Front/todolist/src/routes/createtodo/+page.svelte
@@ -68,163 +68,165 @@
   }
 </script>
 
-<div class="grid grid-cols-4 gap-4">
-  <div>01</div>
-  <div>02</div>
-  {#if option === 0}
-    <div class="col-span-2 container mx-auto py-10">
-      <div class="flex space-x-2 mb-4">
-        <div class="flex-1 mr-2">
-          <input
-            type="text"
-            placeholder="Title"
-            id="todoTitle"
-            class="input input-bordered w-full"
+<div class="p-8">
+  <div class="grid grid-cols-4 gap-4">
+    <div>01</div>
+    <div>02</div>
+    {#if option === 0}
+      <div class="col-span-2 container mx-auto py-10">
+        <div class="flex space-x-2 mb-4">
+          <div class="flex-1 mr-2">
+            <input
+              type="text"
+              placeholder="Title"
+              id="todoTitle"
+              class="input input-bordered w-full"
+            />
+          </div>
+          <button class="btn btn-accent" on:click={clickOption}>Option</button>
+        </div>
+        <div class="flex space-x-2 mb-4">
+          <div class="flex-1 mr-2">
+            <input
+              type="text"
+              placeholder="Description"
+              id="todoDescription"
+              class="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        <div class="flex space-x-2 mb-4">
+          <h1>Jour et heure de la fin de la todolist :</h1>
+          <DateInput
+            bind:date={dateCalendar}
+            class="input input-bordered w-full datepicker-input"
           />
         </div>
-        <button class="btn btn-accent" on:click={clickOption}>Option</button>
-      </div>
-      <div class="flex space-x-2 mb-4">
-        <div class="flex-1 mr-2">
-          <input
-            type="text"
-            placeholder="Description"
-            id="todoDescription"
-            class="input input-bordered w-full"
-          />
-        </div>
-      </div>
-      <div class="flex space-x-2 mb-4">
-        <h1>Jour et heure de la fin de la todolist :</h1>
-        <DateInput
-          bind:date={dateCalendar}
-          class="input input-bordered w-full datepicker-input"
-        />
-      </div>
-      {#if isObjectif === true}
-        <div class="flex space-x-2 mb-4">
-          <div class="flex-1 mr-2">
-            <input
-              type="text"
-              placeholder="Objectif"
-              id="todoObjectif"
-              class="input input-bordered w-full"
-            />
+        {#if isObjectif === true}
+          <div class="flex space-x-2 mb-4">
+            <div class="flex-1 mr-2">
+              <input
+                type="text"
+                placeholder="Objectif"
+                id="todoObjectif"
+                class="input input-bordered w-full"
+              />
+            </div>
           </div>
-        </div>
-      {/if}
-      {#if isGroupe === true}
-        <div class="flex space-x-2 mb-4">
-          <div class="flex-1 mr-2">
-            <input
-              type="text"
-              placeholder="Avec ..."
-              id="todoGroupe"
-              class="input input-bordered w-full"
-            />
+        {/if}
+        {#if isGroupe === true}
+          <div class="flex space-x-2 mb-4">
+            <div class="flex-1 mr-2">
+              <input
+                type="text"
+                placeholder="Avec ..."
+                id="todoGroupe"
+                class="input input-bordered w-full"
+              />
+            </div>
           </div>
-        </div>
-      {/if}
-      {#if isPeriodique === true}
-        <div class="flex space-x-2 mb-4">
-          <div class="flex-1 mr-2">
-            <input
-              type="text"
-              placeholder="Tous les ..."
-              id="todoPeriodique"
-              class="input input-bordered w-full"
-            />
+        {/if}
+        {#if isPeriodique === true}
+          <div class="flex space-x-2 mb-4">
+            <div class="flex-1 mr-2">
+              <input
+                type="text"
+                placeholder="Tous les ..."
+                id="todoPeriodique"
+                class="input input-bordered w-full"
+              />
+            </div>
           </div>
+        {/if}
+        <button class="btn btn-accent" on:click={addTodo}
+          >Ajouter dans Brouillon</button
+        >
+        {#if todos.length === 0}
+          <p>No to-dos added yet.</p>
+        {:else}
+          <ul class="divide-y divide-gray-400">
+            {#each todos as todo, i}
+              <li class="py-4 flex">
+                <div class="flex-1">
+                  <h3 class="text-lg font-bold">{todo.title}</h3>
+                  {#if todo.description != null}<p class="text-gray-500">
+                      {todo.description}
+                    </p>{/if}
+                  {#if todo.date != null}<p class="text-gray-500">
+                      {todo.date}
+                    </p>{/if}
+                  {#if todo.objectif != null}<p class="text-gray-500">
+                      {todo.objectif}
+                    </p>{/if}
+                  {#if todo.groupe != null}<p class="text-gray-500">
+                      {todo.groupe}
+                    </p>{/if}
+                  {#if todo.periodique != null}<p class="text-gray-500">
+                      {todo.periodique}
+                    </p>{/if}
+                </div>
+                <button class="btn btn-warning" on:click={() => removeTodo(i)}
+                  >Remove</button
+                >
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    {:else}
+      <div class="container mx-auto py-10">
+        <div class="flex space-x-2 mb-4">
+          <h1 class="text-5xl font-bold">Option</h1>
+          <button class="btn btn-circle btn-primary" on:click={unclickOption}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              /></svg
+            >
+          </button>
         </div>
-      {/if}
-      <button class="btn btn-accent" on:click={addTodo}
-        >Ajouter dans Brouillon</button
-      >
-      {#if todos.length === 0}
-        <p>No to-dos added yet.</p>
-      {:else}
-        <ul class="divide-y divide-gray-400">
-          {#each todos as todo, i}
-            <li class="py-4 flex">
-              <div class="flex-1">
-                <h3 class="text-lg font-bold">{todo.title}</h3>
-                {#if todo.description != null}<p class="text-gray-500">
-                    {todo.description}
-                  </p>{/if}
-                {#if todo.date != null}<p class="text-gray-500">
-                    {todo.date}
-                  </p>{/if}
-                {#if todo.objectif != null}<p class="text-gray-500">
-                    {todo.objectif}
-                  </p>{/if}
-                {#if todo.groupe != null}<p class="text-gray-500">
-                    {todo.groupe}
-                  </p>{/if}
-                {#if todo.periodique != null}<p class="text-gray-500">
-                    {todo.periodique}
-                  </p>{/if}
-              </div>
-              <button class="btn btn-warning" on:click={() => removeTodo(i)}
-                >Remove</button
-              >
-            </li>
-          {/each}
-        </ul>
-      {/if}
-    </div>
-  {:else}
-    <div class="container mx-auto py-10">
-      <div class="flex space-x-2 mb-4">
-        <h1 class="text-5xl font-bold">Option</h1>
-        <button class="btn btn-circle btn-primary" on:click={unclickOption}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-8 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            ><path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M6 18L18 6M6 6l12 12"
-            /></svg
+        <div class="flex space-x-2 mb-4">
+          <button
+            class={isObjectif ? "btn btn-secondary" : "btn btn-primary"}
+            on:click={objectifClick}
           >
-        </button>
+            Objectif
+          </button>
+          <h1>Ajout d'un objectif</h1>
+        </div>
+        <div class="flex space-x-2 mb-4">
+          <button
+            class={isGroupe ? "btn btn-secondary" : "btn btn-primary"}
+            on:click={groupeClick}
+          >
+            Groupe
+          </button>
+          <button
+            class={isGroupe ? "btn btn-primary" : "btn btn-secondary"}
+            on:click={groupeClick}
+          >
+            Solo
+          </button>
+          <h1>Est ce un todo de groupe où un todo simple?</h1>
+        </div>
+        <div class="flex space-x-2 mb-4">
+          <button
+            class={isPeriodique ? "btn btn-secondary" : "btn btn-primary"}
+            on:click={periodiqueClick}
+          >
+            Periodique
+          </button>
+          <h1>Est ce un todo periodique?</h1>
+        </div>
       </div>
-      <div class="flex space-x-2 mb-4">
-        <button
-          class={isObjectif ? "btn btn-secondary" : "btn btn-primary"}
-          on:click={objectifClick}
-        >
-          Objectif
-        </button>
-        <h1>Ajout d'un objectif</h1>
-      </div>
-      <div class="flex space-x-2 mb-4">
-        <button
-          class={isGroupe ? "btn btn-secondary" : "btn btn-primary"}
-          on:click={groupeClick}
-        >
-          Groupe
-        </button>
-        <button
-          class={isGroupe ? "btn btn-primary" : "btn btn-secondary"}
-          on:click={groupeClick}
-        >
-          Solo
-        </button>
-        <h1>Est ce un todo de groupe où un todo simple?</h1>
-      </div>
-      <div class="flex space-x-2 mb-4">
-        <button
-          class={isPeriodique ? "btn btn-secondary" : "btn btn-primary"}
-          on:click={periodiqueClick}
-        >
-          Periodique
-        </button>
-        <h1>Est ce un todo periodique?</h1>
-      </div>
-    </div>
-  {/if}
+    {/if}
+  </div>
 </div>
