c195023da0674c740fc1dbd1b74dc0ac274efbcf
Kyuzial <kyuzial@protonmail.com>
2023-05-06 18:35:32 +0200

    front: ajout recherche + liste todo

--- Front/todolist/src/routes/createtodo/+page.svelte
+++ Front/todolist/src/routes/createtodo/+page.svelte
@@ -70,10 +70,76 @@
 
 <div class="p-8">
   <div class="grid grid-cols-4 gap-4">
-    <div>01</div>
-    <div>02</div>
+    <div>
+      <form>
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >Search</label
+        >
+        <div class="relative">
+          <div
+            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              /></svg
+            >
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search Todos"
+            required
+          />
+          <button
+            type="submit"
+            class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >Search</button
+          >
+        </div>
+      </form>
+      <div class="p-3">
+        <div class="flex mb-4 items-center">
+          <p class="w-full">Todo1</p>
+          <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded"
+            >Done</button
+          >
+          <button class="flex-no-shrink p-2 ml-2 border-2 rounded"
+            >Remove</button
+          >
+          <button class="flex-no-shrink p-2 ml-2 border-2 rounded"
+            >Informations</button
+          >
+        </div>
+        <div class="flex mb-4 items-center">
+          <p class="w-full line-through">Todo2</p>
+          <button class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded"
+            >Not Done</button
+          >
+          <button class="flex-no-shrink p-2 ml-2 border-2 rounded"
+            >Remove</button
+          >
+          <button class="flex-no-shrink p-2 ml-2 border-2 rounded"
+            >Informations</button
+          >
+        </div>
+      </div>
+    </div>
+    <div class="col-span-2 container mx-auto py-10">01</div>
     {#if option === 0}
-      <div class="col-span-2 container mx-auto py-10">
+      <div class="col-span-1 container mx-auto py-10">
         <div class="flex space-x-2 mb-4">
           <div class="flex-1 mr-2">
             <input
