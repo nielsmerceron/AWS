2316d6a0403904f94726a0c3e05dcc794bb20fec
Kyuzial <kyuzial@protonmail.com>
2023-05-14 00:56:08 +0200

    Front: Make search simpler

--- Front/todolist/src/routes/createtodo/search.js
+++ Front/todolist/src/routes/createtodo/search.js
@@ -21,35 +21,7 @@ export { Todoget };
  * @param {any} result
  */
 function trimbytitle(titre, result) {
-  let todoList = [
-    {
-      id: "",
-      title: "test",
-      description: "",
-      dated: new Date(),
-      datee: new Date(),
-      status: false,
-    },
-  ];
-  if (result != null) {
-    let arrayresult = result.parse();
-    for (let i = 1; i < arrayresult.lenght; i = i + 7) {
-      if (arrayresult.at(i) === titre) {
-        todoList = [
-          ...todoList,
-          {
-            id: arrayresult.at(i - 1),
-            title: arrayresult.at(i),
-            description: arrayresult.at(i + 1),
-            dated: arrayresult.at(i + 2),
-            datee: arrayresult.at(i + 3),
-            status: arrayresult.at(i + 4),
-          },
-        ];
-      }
-    }
-  }
-  return todoList;
+  return result.filter((x) => x.title === titre);
 }
 
 export { trimbytitle };
